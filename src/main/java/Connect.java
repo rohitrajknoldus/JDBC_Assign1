ff38c1df41eaf6dc1d79e6606ef0fd017ddb69b1
Rohit Raj <rohit.raj@knoldus.com>
2021-11-02 19:04:29 +0530

    first commit

---
+++ src/main/java/Connect.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+class Connect
+{
+    public static void main(String[] args) {
+        try
+        {
+            final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+            final String DB_URL = "jdbc:mysql://localhost:3306/Raj";
+            final String USER = "root";
+            final String PASS = "Rohit@122333";
+
+            Class.forName(JDBC_DRIVER);
+            Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            Statement s = conn.createStatement();
+            ResultSet res = s.executeQuery("select sum(Amount) as total from Cart");
+            while(res.next())
+                System.out.println("Total amount to be paid = "+res.getString(1));
+            Statement stmt2=conn.createStatement();
+            ResultSet result =stmt2.executeQuery("select pname from Product where pid=(select pid from Cart where qty=(select Max(qty) from Cart));");
+            while(result.next())
+                System.out.println("Maximum sold = "+result.getString(1));
+            System.out.println("List all the Products which are not yet sold.\n");
+            Statement stmt3=conn.createStatement();
+            ResultSet r3=stmt3.executeQuery("select pname from Product where pid NOT IN(Select pid from Cart)");
+            while(r3.next())
+                System.out.println(r3.getString(1));
+
+            System.out.println("\n\n");
+            conn.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
